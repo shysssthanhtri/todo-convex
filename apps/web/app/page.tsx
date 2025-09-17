bdad25813b8851fe22586e264f7eb72395f464d0
shysssthanhtri <shyssthanhtri@gmail.com>
2025-09-17 18:37:07 +0700

    feat: add backend into web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { Button } from "@workspace/ui/components/button";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/convex";
 
 /**
  * Renders a centered page with a "Hello World" heading and a small Button.
@@ -8,10 +12,16 @@ import { Button } from "@workspace/ui/components/button";
  * @returns The JSX element for this page.
  */
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
+        <ul>
+          {users?.map((user) => (
+            <li key={user._id}>{user.name}</li>
+          ))}
+        </ul>
         <Button size="sm">Button</Button>
       </div>
     </div>
