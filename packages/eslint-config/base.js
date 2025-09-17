dd21b70b77c53c14cbb250b214ea9fec8ca2e255
shysssthanhtri <shyssthanhtri@gmail.com>
2025-09-17 07:30:29 +0700

    feat: add eslint plugins

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,10 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import onlyWarn from "eslint-plugin-only-warn"
-import turboPlugin from "eslint-plugin-turbo"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +18,24 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
   {
@@ -29,4 +46,4 @@ export const config = [
   {
     ignores: ["dist/**"],
   },
-]
+];
